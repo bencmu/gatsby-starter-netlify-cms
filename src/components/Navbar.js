b1afd7c619504234cb9d96c4d699286770b79719
Panat <panatmaki@gmail.com>
2020-04-12 14:11:39 +0700

    comment_out_statusBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -66,7 +66,7 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 Test
               </Link>
-              <Link className="navbar-item" to="/products">
+              {/* <Link className="navbar-item" to="/products">
                 Products
               </Link>
               <Link className="navbar-item" to="/blog">
@@ -77,7 +77,7 @@ const Navbar = class extends React.Component {
               </Link>
               <Link className="navbar-item" to="/contact/examples">
                 Form Examples
-              </Link>
+              </Link> */}
             </div>
             <div className="navbar-end has-text-centered">
               <a
